8c1ad69cdca5a951a4b15770b0ddf0ded3ed22d8
XTutorialsOfficial <xtutorials161@gmail.com>
2017-11-27 00:30:46 +0200

    upload hardware configs and others

---
+++ prebuilts/Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := usbaudio.kl
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_KEYLAYOUT)
+include $(BUILD_PREBUILT)
