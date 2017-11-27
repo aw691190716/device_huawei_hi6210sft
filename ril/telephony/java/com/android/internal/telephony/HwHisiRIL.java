42781a1844e64f84ef03d46a8856e4ee1ea974a0
XTutorialsOfficial <xtutorials161@gmail.com>
2017-11-27 20:19:08 +0200

    alice: fix signal strength

--- ril/telephony/java/com/android/internal/telephony/HwHisiRIL.java
+++ ril/telephony/java/com/android/internal/telephony/HwHisiRIL.java
@@ -801,43 +801,25 @@ public class HwHisiRIL extends RIL implements CommandsInterface {
 
         Rlog.e(RILJ_LOG_TAG, "responseSignalStrength called");
 
-        int[] response = new int[13];
-        for (int i = 0; i < 13; i++) {
+        int[] response = new int[14];
+        for (int i = 0; i < 14; i++) {
             response[i] = p.readInt();
         }
-        int gsmSignalStrength = response[0]; /* Valid values are (0-31, 99) */
-
-        int gsmBitErrorRate = response[1]; /* bit error rate (0-7, 99) */
-
-        int cdmaDbm = response[2]; /* Valid values are positive integers.
-                                    This value is the actual RSSI value multiplied by -1. */
-
-        int cdmaEcio = response[3]; /* Valid values are positive integers.
-                                    This value is the actual Ec/Io multiplied by -10. */
-
-        int evdoDbm = response[4]; /* Valid values are positive integers.
-                                    This value is the actual RSSI value multiplied by -1. */
-
-        int evdoEcio = response[5]; /* Valid values are positive integers.
-                                    This value is the actual Ec/Io multiplied by -10. */
-
-        int evdoSnr = response[6]; /* Valid values are 0-8.  8 is the highest signal to noise ratio. */
-
-        int lteSignalStrength = response[7]; /* Valid values are (0-31, 99)*/
-
-        int lteRsrp = response[8]; /* The current Reference Signal Receive Power
-                                    in dBm multipled by -1. Range: 44 to 140 dBm */
-
-        int lteRsrq = response[9]; /* The current Reference Signal Receive Quality
-                                    in dB multiplied by -1 Range: 20 to 3 dB. */
-
-        int lteRssnr = response[10]; /* The current reference signal signal-to-noise ratio
-                                    in 0.1 dB units. Range: -200 to +300 */
-
-        int lteCqi = response[11]; /* The current Channel Quality Indicator. Range: 0 to 15. */
-        int gsm = response[12];
-        int TdScdmaRscp = 0;
         boolean bGsm = false;
+	int gsmSignalStrength = response[0];
+        int gsmBitErrorRate = response[1];
+        int wcdmaRscp = response[2];
+        int wcdmaEcio = response[3];
+        int cdmaDbm = response[4];
+        int cdmaEcio = response[5];
+        int evdoDbm = response[6];
+        int evdoEcio = response[7];
+        int evdoSnr = response[8];                                                                   
+	int lteSignalStrength = response[9];
+        int lteRsrp = response[10];
+        int lteRsrq = response[11];
+        int lteRssnr = response[12];
+        int lteCqi = response[13];
 
         Rlog.d(RILJ_LOG_TAG, "---------- Lte Values ----------");
         Rlog.d(RILJ_LOG_TAG, "lteSignalStrength:" + lteSignalStrength);
@@ -850,7 +832,6 @@ public class HwHisiRIL extends RIL implements CommandsInterface {
         Rlog.d(RILJ_LOG_TAG, "---------- Other Values ----------");
         Rlog.d(RILJ_LOG_TAG, "cdmaDbm:" + cdmaDbm);
         Rlog.d(RILJ_LOG_TAG, "cdmaEcio:" + cdmaEcio);
-        Rlog.d(RILJ_LOG_TAG, "gsm:" + gsm);
         Rlog.d(RILJ_LOG_TAG, "gsmSignalStrength:" + gsmSignalStrength);
         Rlog.d(RILJ_LOG_TAG, "evdoDbm:" + evdoDbm);
         Rlog.d(RILJ_LOG_TAG, "-------------------------");
@@ -926,6 +907,16 @@ public class HwHisiRIL extends RIL implements CommandsInterface {
         }
 
         bGsm = gsm != 0;
+	    
+	if (gsmSignalStrength != -1) {
+             if (DBG) {
+                 Rlog.i(RILJ_LOG_TAG, ": original gsmSignalStrength: " + gsmSignalStrength);
+             }
+             gsmSignalStrength = -(gsmSignalStrength - 113) / 2;
+ 	}
+ 
+ 	SignalStrength sst = new SignalStrength(gsmSignalStrength, gsmBitErrorRate, cdmaDbm, cdmaEcio, evdoDbm, evdoEcio, evdoSnr,
+             lteSignalStrength, lteRsrp, lteRsrq, lteRssnr, lteCqi, wcdmaRscp, true);    
 
     SignalStrength signalStrength = new SignalStrength(gsmSignalStrength, gsmBitErrorRate, cdmaDbm, cdmaEcio, evdoDbm, evdoEcio, evdoSnr,
             lteSignalStrength, lteRsrp, lteRsrq, lteRssnr, lteCqi, TdScdmaRscp, bGsm);
