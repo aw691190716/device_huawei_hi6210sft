b8fd387fca1da600dda3cde7014378035fdbd8d6
XTutorialsOfficial <xtutorials161@gmail.com>
2017-11-27 23:12:07 +0200

    alice: try to fix compilling issue

--- Android.mk
+++ Android.mk
@@ -1,12 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),hi6210sft)
-include $(call all-makefiles-under,$(LOCAL_PATH)
+ifeq ($(TARGET_DEVICE), hi6210sft)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
 endif
-
-include $(CLEAR_VARS)
-
-ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
-
-# include the non-open-source counterpart to this file
--include vendor/huawei/hi6210sft/AndroidBoardVendor.mk
