a80150c9f602a0b702311142599c0a4a035e94f6
XTutorialsOfficial <xtutorials161@gmail.com>
2017-11-27 23:08:49 +0200

    alice: try to fix compiling issue

--- aokp.mk
+++ aokp.mk
@@ -1,15 +1,5 @@
-# Release name
-PRODUCT_RELEASE_NAME := hi6210sft
+$(call inherit-product, device/huawei/hi6210sft/full_hi6210sft.mk)
 
-# Inherit some common AOKP stuff.
-$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
-
-# Inherit device configuration
-$(call inherit-product, device/huawei/hi6210sft/device_hi6210sft.mk)
-
-## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := hi6210sft
 PRODUCT_NAME := aokp_hi6210sft
-PRODUCT_BRAND := huawei
-PRODUCT_MODEL := hi6210sft
-PRODUCT_MANUFACTURER := huawei
+
